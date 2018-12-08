eb27aef4a31adad36c14792e0e55801034756912
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-12-08 11:44:08 -0700

    Post request working for action

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -22,4 +22,19 @@ router.get('/:id', (req, res) => {
         });
 });
 
+router.post('/', (req, res) => {
+    const action = req.body;
+    if (action.description && action.notes && action.project_id) {
+        actionDb.insert(action)
+            .then((action) => {
+                res.status(201).json(action);
+            })
+            .catch((err) => {
+                res.status(500).json({error: "Unable to create action."});
+            });
+    } else {
+        res.status(400).json({errorMessage: "Please provide 'description', 'notes', and 'project_id' for the action."});
+    }
+});
+
 module.exports = router;
